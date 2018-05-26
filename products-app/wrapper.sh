6e421bd03ae586663936a0ed18578040e4e5428e
Venkat Vallabhaneni <venkata.vallabhaneni@gmail.com>
2018-05-27 00:52:51 +0530

    containerization

---
+++ products-app/wrapper.sh
@@ -0,0 +1,6 @@
+while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
+    echo "Trying to connect to postgres sql at ${DATABASE_PORT}..."
+    sleep 10
+done
+
+java  -Dspring.profiles.active=container -jar /products.jar
